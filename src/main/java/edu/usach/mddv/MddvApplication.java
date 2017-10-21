8302444211c8ec0a1ceacebae5b687bb4fc80700
prulloac <pablo.ulloac@usach.cl>
2017-10-21 17:55:41 -0300

    testing tomcat deployment via shell

--- src/main/java/edu/usach/mddv/MddvApplication.java
+++ src/main/java/edu/usach/mddv/MddvApplication.java
@@ -1,10 +1,18 @@
 package edu.usach.mddv;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MddvApplication {
+public class MddvApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MddvApplication.class, args);
