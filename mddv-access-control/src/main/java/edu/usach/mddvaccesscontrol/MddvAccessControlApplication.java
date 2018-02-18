614f9984b23b7fb9272fb2b86321c823b98ae06b
prulloac <pablo.ulloac@usach.cl>
2018-02-18 12:11:48 -0300

    initial commit for access-control server

---
+++ mddv-access-control/src/main/java/edu/usach/mddvaccesscontrol/MddvAccessControlApplication.java
@@ -0,0 +1,12 @@
+package edu.usach.mddvaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MddvAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MddvAccessControlApplication.class, args);
+	}
+}
