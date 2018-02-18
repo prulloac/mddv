da728cf8f19f6ca600da966a5675d1e7740cfa22
prulloac <pablo.ulloac@usach.cl>
2018-02-18 12:12:20 -0300

    initial commit for api gateway server

---
+++ mddv-api-gateway/src/main/java/edu/usach/mddvapigateway/MddvApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package edu.usach.mddvapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MddvApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MddvApiGatewayApplication.class, args);
+	}
+}
---
+++ mddv-api-gateway/src/test/java/edu/usach/mddvapigateway/MddvApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.usach.mddvapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MddvApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
