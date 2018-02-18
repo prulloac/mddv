853cff365c851bdbafbb3bd2fcfef78cbd31a9c5
prulloac <pablo.ulloac@usach.cl>
2018-02-18 13:47:50 -0300

    base config passed integration test

--- mddv-access-control/src/test/java/edu/usach/mddvaccesscontrol/AccessControlServerIntegrationTest.java
+++ mddv-access-control/src/test/java/edu/usach/mddvaccesscontrol/AccessControlServerIntegrationTest.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class MddvAccessControlApplicationTests {
+@SpringBootTest(classes = MddvAccessControlApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AccessControlServerIntegrationTest {
 
 	@Test
-	public void contextLoads() {
+	public void whenLoadApplication_thenSuccess() {
 	}
 
 }
