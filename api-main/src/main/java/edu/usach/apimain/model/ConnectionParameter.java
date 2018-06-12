be96868f738935d6bdf32a6d79ff1de59f3af294
prulloac <pablo.ulloac@usach.cl>
2018-06-12 12:57:37 -0400

    updated connectionParameter parent class

--- api-main/src/main/java/edu/usach/apimain/model/ConnectionParameter.java
+++ api-main/src/main/java/edu/usach/apimain/model/ConnectionParameter.java
@@ -1,6 +1,7 @@
 package edu.usach.apimain.model;
 
 import edu.usach.apicommons.model.IEntity;
+import edu.usach.apicommons.model.impl.AbstractIdentityEntity;
 import edu.usach.apicommons.model.impl.AbstractNamedEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,9 +15,10 @@ import javax.persistence.ManyToOne;
 @Getter
 @Setter
 @ToString
-public class ConnectionParameter extends AbstractNamedEntity implements IEntity {
+public class ConnectionParameter extends AbstractIdentityEntity implements IEntity {
   private static final long serialVersionUID = 1L;
 
+  private String name;
 	private String value;
 	@ManyToOne
 	@JoinColumn(name = "repositoryId")
