b4465127306ba4badc32a6111dffb187d1d8babc
prulloac <pablo.ulloac@usach.cl>
2018-06-17 18:01:47 -0400

    initialized businessObjectService

---
+++ ui/src/services/BusinessObjectService.js
@@ -0,0 +1,57 @@
+import httpClient from './HttpClient'
+
+const create = ({
+  name = '',
+  description = '',
+  type = '',
+  version = '',
+}) =>
+  httpClient.post({
+    endpoint: 'business-objects',
+    data: {
+      name,
+      description,
+      type,
+      version,
+    },
+  })
+
+const update = ({
+  id = 0,
+  name = '',
+  description = '',
+  type = '',
+  version = '',
+}) =>
+  httpClient.put({
+    endpoint: '/business-objects',
+    data: {
+      id,
+      name,
+      description,
+      type,
+      version,
+    },
+  })
+
+const del = (id = 0) =>
+  httpClient.delete({
+    endpoint: `business-objects/?id=${id}`,
+  })
+
+const findById = (id = 0) => httpClient.get({ endpoint: `business-objects/${id}` })
+
+const findByName = (name = 0) => httpClient.get({ endpoint: `business-objects?name=${name}` })
+
+const getAll = () => httpClient.get({ endpoint: 'business-objects?showAll=true' })
+
+const BusinessObjectService = {
+  create,
+  findById,
+  findByName,
+  getAll,
+  update,
+  delete: del,
+}
+
+export default BusinessObjectService
