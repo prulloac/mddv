638cc9f59ac42b9f528bcee1c05e0eb75cbeaa80
prulloac <pablo.ulloac@usach.cl>
2018-06-19 14:59:14 -0400

    added technical object service

---
+++ ui/src/services/TechnicalObjectService.js
@@ -0,0 +1,61 @@
+import httpClient from './HttpClient'
+
+const create = ({
+  name = '',
+  description = '',
+  type = '',
+  version = '',
+}) =>
+  httpClient.post({
+    endpoint: 'technical-objects',
+    data: {
+      name,
+      description,
+      type,
+      version,
+    },
+  })
+
+const update = ({
+  id = 0,
+  name = '',
+  description = '',
+  type = '',
+  version = '',
+}) =>
+  httpClient.put({
+    endpoint: '/technical-objects',
+    data: {
+      id,
+      name,
+      description,
+      type,
+      version,
+    },
+  })
+
+const del = (id = 0) =>
+  httpClient.delete({
+    endpoint: `technical-objects/?id=${id}`,
+  })
+
+const findById = (id = 0) => httpClient.get({ endpoint: `technical-objects/${id}` })
+
+const findByName = (name = 0) => httpClient.get({ endpoint: `technical-objects?name=${name}` })
+
+const getAll = () => httpClient.get({ endpoint: 'technical-objects?showAll=true' })
+
+const getTypes = () => httpClient.get({ endpoint: 'technical-objects/types' })
+
+
+const TechnicalObjectService = {
+  create,
+  findById,
+  findByName,
+  getAll,
+  update,
+  delete: del,
+  getTypes,
+}
+
+export default TechnicalObjectService
