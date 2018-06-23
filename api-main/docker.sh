f7e6075a14636300d40621bcf382eed9ef854caf
prulloac <pablo.ulloac@usach.cl>
2018-06-22 20:54:12 -0400

    migrated to mysql database engine

--- api-main/docker.sh
+++ api-main/docker.sh
@@ -19,3 +19,5 @@ fi
 
 echo "launching container"
 docker run -v mvn-repo:/root/.m2 -v h2-main:/root/h2Databases --name $API -d -p $PORT:$PORT $TAG
+
+docker run --name main-data -e MYSQL_ROOT_PASSWORD=$API -e MYSQL_DATABASE=$TAG -p 3301:3306 -v main-data:/var/lib/mysql -d mysql:5.7
