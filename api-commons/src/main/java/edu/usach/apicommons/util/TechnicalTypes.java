fc48ed2eab10727f1e8781e89fe0dd760ff94048
Pablo Ulloa <pablo.ulloac@usach.cl>
2018-07-02 14:07:33 -0400

    improved technical types with hierarchy

--- api-commons/src/main/java/edu/usach/apicommons/util/TechnicalTypes.java
+++ api-commons/src/main/java/edu/usach/apicommons/util/TechnicalTypes.java
@@ -6,25 +6,24 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TechnicalTypes {
-	REPOSITORY("Repository", "Repositorio"),
-	RDBMS("Relational Database", "Base de datos relacional"),
-	SCHEMA("Schema", "Esquema"),
-	TABLE("Table", "Tabla"),
-	COLUMN("Column", "Columna"),
-	DOCUMENT_DB("Document Database", "Base de datos de documentos"),
-	COLLECTION("Collection", "Colección"),
-	COLLECTION_ATTRIBUTE("Collection attribute", "Atributo de colección"),
-	DOCUMENT("Document", "Documento"),
-	FILE("File", "Archivo"),
-	SHEET("Sheet", "Planilla"),
-	FOLDER("Folder", "Carpeta"),
-	RELATION("Relation", "Relación"),
-	RELATION_MANY_ONE("Relation Many to One", "Relación Muchos a Uno"),
-	RELATION_MANY_MANY("Relation Many to Many", "Relación Muchos a Muchos"),
-	RELATION_DEPENDENCY("Dependency Relation", "Relación de Dependencia"),
-	RELATION_USE("Use Relation", "Relación de Uso"),
-	RELATION_GENERALIZATION("Generalization Relation", "Relación de Generalización");
+	REPOSITORY("Repository", "Repositorio", null),
+	RDBMS("Relational Database", "Base de datos relacional", REPOSITORY),
+	TABLE("Table", "Tabla", RDBMS),
+	COLUMN("Column", "Columna", TABLE),
+	DOCUMENT_DB("Document Database", "Base de datos de documentos", REPOSITORY),
+	COLLECTION("Collection", "Colección", DOCUMENT_DB),
+	COLLECTION_ATTRIBUTE("Collection attribute", "Atributo de colección", COLLECTION),
+	FOLDER("Folder", "Carpeta", REPOSITORY),
+	DOCUMENT("Document", "Documento", FOLDER),
+	FILE("File", "Archivo", FOLDER),
+	SHEET("Sheet", "Planilla", FOLDER),
+	RELATION("Relation", "Relación", null),
+	RELATION_MANY_ONE("Relation Many to One", "Relación Muchos a Uno", TABLE),
+	RELATION_DEPENDENCY("Dependency Relation", "Relación de Dependencia", COLLECTION),
+	RELATION_USE("Use Relation", "Relación de Uso", COLLECTION),
+	RELATION_GENERALIZATION("Generalization Relation", "Relación de Generalización", COLLECTION);
 
 	private String type;
 	private String translation;
+	private TechnicalTypes parent;
 }
